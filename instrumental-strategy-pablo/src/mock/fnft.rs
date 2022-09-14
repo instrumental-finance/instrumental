423dabd1d4cc2cc53d1068002b07910a8413680c
saruman9 <rum.274.4@gmail.com>
2022-09-14 11:01:28 +0800

    style(instrumental*): fix clippy's warnings

--- instrumental-strategy-pablo/src/mock/fnft.rs
+++ instrumental-strategy-pablo/src/mock/fnft.rs
@@ -14,17 +14,17 @@ impl Inspect<AccountId> for MockFnft {
     type ItemId = ();
 
     fn owner(_collection: &Self::CollectionId, _item: &Self::ItemId) -> Option<AccountId> {
-        todo!()
+        unimplemented!()
     }
 }
 
 impl FinancialNft<AccountId> for MockFnft {
     fn asset_account(_collection: &Self::CollectionId, _instance: &Self::ItemId) -> AccountId {
-        todo!()
+        unimplemented!()
     }
 
     fn get_next_nft_id(_collection: &Self::CollectionId) -> Result<Self::ItemId, DispatchError> {
-        todo!()
+        unimplemented!()
     }
 }
 
@@ -34,7 +34,7 @@ impl Create<AccountId> for MockFnft {
         _who: &AccountId,
         _admin: &AccountId,
     ) -> DispatchResult {
-        todo!()
+        unimplemented!()
     }
 }
 
