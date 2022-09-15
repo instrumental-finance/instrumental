ComposableFi/composable

Feature 1 (whitelist_strategy) CU-25k5ny0[COMPLETE] Validity Checks CU-270twfz[COMPLETE] StrategyAlreadyWhitelisted CU-270twk2[COMPLETE] TooManyWhitelistedStrategies CU-270twk4[COMPLETE] Runtime Storage Updates CU-270twgv[COMPLETE] WhitelistedStrategies CU-270twnm[COMPLETE] Automated Tests CU-270twh2[COMPLETE] Able to whitelist strategies CU-270tx05[COMPLETE] Duplicate whitelisted strategies raises an error CU-270tx6m[COMPLETE]

frame/instrumental-strategy/src/mock/strategies.rs
@@ -1,0 +1,38 @@
+use frame_support::PalletId;
+use sp_runtime::traits::AccountIdConversion;
+
+use super::runtime::AccountId;
+
+#[derive(Clone, Copy)]
+pub struct Strategy {
+    pub pallet_id: PalletId,
+}
+
+impl Strategy {
+    const fn new(pallet_id: PalletId) -> Strategy {
+        Strategy {
+            pallet_id: pallet_id,
+        }
+    }
+
+    pub fn account_id(self: Strategy) -> AccountId {
+        self.pallet_id.into_account()
+    }
+}
+
+// separate module so that the `allow` attribute isn't applied to the entirety of the Strategy
+// module.
+pub mod defs {
+	#![allow(clippy::upper_case_acronyms)]
+    #![allow(non_camel_case_types)]
+    #![allow(unused)]
+
+    use super::super::runtime::AccountId;
+
+    use super::Strategy;
+	use frame_support::PalletId;
+
+    pub const PABLO_STRATEGY: Strategy = Strategy::new(PalletId(*b"stratpab"));
+}
+
+pub use defs::*;

instrumental-finance/instrumental

instrumental-strategy/src/mock/strategies.rs
@@ -1,0 +1,38 @@
+use frame_support::PalletId;
+use sp_runtime::traits::AccountIdConversion;
+
+use super::runtime::AccountId;
+
+#[derive(Clone, Copy)]
+pub struct Strategy {
+    pub pallet_id: PalletId,
+}
+
+impl Strategy {
+    const fn new(pallet_id: PalletId) -> Strategy {
+        Strategy {
+            pallet_id: pallet_id,
+        }
+    }
+
+    pub fn account_id(self: Strategy) -> AccountId {
+        self.pallet_id.into_account_truncating()
+    }
+}
+
+// separate module so that the `allow` attribute isn't applied to the entirety of the Strategy
+// module.
+pub mod defs {
+	#![allow(clippy::upper_case_acronyms)]
+    #![allow(non_camel_case_types)]
+    #![allow(unused)]
+
+    use super::super::runtime::AccountId;
+
+    use super::Strategy;
+	use frame_support::PalletId;
+
+    pub const PABLO_STRATEGY: Strategy = Strategy::new(PalletId(*b"stratpab"));
+}
+
+pub use defs::*;